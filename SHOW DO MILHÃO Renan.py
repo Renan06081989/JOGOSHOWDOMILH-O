36f5c8f96cc1187063f1aff24363c3714cfa0761
RENAN CALDEIRA DE ANDRADE <113741243+Renan06081989@users.noreply.github.com>
2023-03-10 10:28:24 -0300

    JOGO SHOW DO MILHÃO PARA DISCIPLINA DE ALP
    JOGO SHOW DO MILHAO PARA DISCIPLINA DE ALP USANDO LINGUAGEM PYTHON

---
+++ SHOW DO MILHÃO Renan.py
@@ -0,0 +1,338 @@
+def menu():
+    print("$"*80)
+    print('''
+                                SHOW DO MILHÃO\n''')
+    print("$"*80)
+    print()
+    import time
+    time.sleep(2)
+    print('''                                   ALUNO:''')
+    import time
+    time.sleep(2)
+    print('''                                   Renan''')
+    print("VAMOS COMEÇAR!")
+menu()
+
+import random
+
+perguntasNivelA=[]
+perguntasNivelA.append("Qual é o nome dado ao estado da água em forma de gelo?")
+perguntasNivelA.append("Quanto é 2+2?")
+perguntasNivelA.append("Quanto é 3+3?")
+perguntasNivelA.append("Quem é a namorada do Mickey?")
+perguntasNivelA.append("Qual é o personagem do folclore brasileiro que tem uma perna só?")
+
+alternativaNivelA=[]
+alternativaNivelA.append('\na)Líquido\nb)Sólido\nc)Gasoso\nd)Vaporoso\n')
+alternativaNivelA.append('\na)11\nb)9\nc)4\nd)09\n')
+alternativaNivelA.append('\na)10\nb)8\nc)6\nd)29\n')
+alternativaNivelA.append('\na)Margarida\nb)Minnie\nc)A pequena sereia\nd)Olívia Palito\n')
+alternativaNivelA.append('\na)Cuca\nb)Negrinho do pastoreio\nc)Boitatá\nd)Saci-Pererê\n')
+
+respostasNivelA=[]
+respostasNivelA.append("B")
+respostasNivelA.append("C")
+respostasNivelA.append("C")
+respostasNivelA.append("B")
+respostasNivelA.append("D")
+
+perguntasNivelB=[]
+perguntasNivelB.append("Que imperador pôs fogo em Roma??")
+perguntasNivelB.append("Qual a capital da Paraíba?")
+perguntasNivelB.append("Qual a montanha mais alta do mundo??")
+perguntasNivelB.append("Onde se localiza Machu Picchu?")
+perguntasNivelB.append("Quanto é 50+50?")
+
+alternativaNivelB=[]
+alternativaNivelB.append('\na)Trajano\nb)Nero\nc)Brutus\nd)Calígula\n')
+alternativaNivelB.append('\na)São Paulo\nb)Rio de Janeiro\nc)João Pessoa\nd)Sumé\n')
+alternativaNivelB.append('\na)Mauna Kea\nb)Dhaulagiri\nc)Monte Chimborazo\nd) Monte Everest\n')
+alternativaNivelB.append('\na)Colômbia\nb)Peru\nc)China\nd)Bolívia\n')
+alternativaNivelB.append('\na)3\nb)8\nc)100\nd)9\n')
+
+respostasNivelB=[]
+respostasNivelB.append("B")
+respostasNivelB.append("C")
+respostasNivelB.append("D")
+respostasNivelB.append("B")
+respostasNivelB.append("C")
+
+perguntasNivelC=[]
+perguntasNivelC.append("Que país tem o formato de uma bota?")
+perguntasNivelC.append("A que temperatura a água ferve?")
+perguntasNivelC.append("Qual o maior planeta do sistema solar?")
+perguntasNivelC.append("Qual o planeta mais próximo do Sol?")
+perguntasNivelC.append("Quantos continentes existem?")
+
+alternativaNivelC=[]
+alternativaNivelC.append('\na)Butão\nb)Brasil\nc)Portugal\nd)Itália\n')
+alternativaNivelC.append('\na)200ºC\nb)-10ºC\nc)180ºC\nd)100ºC\n')
+alternativaNivelC.append('\na)Marte\nb)Lua\nc)Saturno\nd)Júpiter\n')
+alternativaNivelC.append('\na)Netuno\nb)Júpiter\nc)Mercúrio\nd)Terra\n')
+alternativaNivelC.append('\na)2\nb)8\nc)4\nd)6\n')
+
+respostasNivelC=[]
+respostasNivelC.append("D")
+respostasNivelC.append("D")
+respostasNivelC.append("D")
+respostasNivelC.append("C")
+respostasNivelC.append("D")
+
+perguntasNivelD=[]
+perguntasNivelD.append("Qual a maior floresta tropical do mundo?")
+perguntasNivelD.append("Qual a língua mais falada no mundo?")
+perguntasNivelD.append("Qual o monumento famoso pela sua inclinação?")
+perguntasNivelD.append("Kryptonita é a fraqueza de qual super-herói?")
+perguntasNivelD.append("Quem pintou Mona Lisa?")
+
+alternativaNivelD=[]
+alternativaNivelD.append('\na)Mata Atlântica\nb)Pampas\nc)Pantanal\nd)Floresta Amazônica\n')
+alternativaNivelD.append('\na)espanhol\nb)português\nc)chinês\nd)Inglês\n')
+alternativaNivelD.append('\na)Torre de Pisa\nb)Esfinge\nc)Estátua da Liberdade\nd)Cristo Redentor\n')
+alternativaNivelD.append('\na)Batman\nb)Hulk\nc)Super-Homem\nd)Flash\n')
+alternativaNivelD.append('\na)Salvador Dalí\nb)Van Gogh\nc)Tarsila do Amaral\nd)Leonardo da Vinci\n')
+
+respostasNivelD=[]
+respostasNivelD.append("D")
+respostasNivelD.append("D")
+respostasNivelD.append("A")
+respostasNivelD.append("C")
+respostasNivelD.append("D")
+
+perguntasNivelE=[]
+perguntasNivelE.append("Qual a personagem mais famosa de Maurício de Sousa?")
+perguntasNivelE.append("Que animal põe o maior ovo?")
+perguntasNivelE.append("Quantos andares tem o maior prédio do mundo?")
+perguntasNivelE.append("Em que país foi construído o Muro de Berlim?")
+perguntasNivelE.append("Em que ano foi usado um celular pela primeira vez no Brasil?")
+
+alternativaNivelE=[]
+alternativaNivelE.append("\na)Mafalda\nb)Mônica\nc)Smurfette\nd)Menino Maluquinho\n")
+alternativaNivelE.append("\na)Beija-flor\nb)Avestruz\nc)Tartaruga-gigante\nd)Pavão\n")
+alternativaNivelE.append("\na)100\nb)200\nc)163\nd)50\n")
+alternativaNivelE.append("\na)Estados Unidos\nb)China\nc)Coreia do Norte\nd)Alemanha\n")
+alternativaNivelE.append("\na)1900\nb)1990\nc)1890\nd)1901\n")
+
+respostasNivelE=[]
+respostasNivelE.append("B")
+respostasNivelE.append("B")
+respostasNivelE.append("C")
+respostasNivelE.append("D")
+respostasNivelE.append("B")
+
+
+
+perguntasFeitas = []
+contadorDePulos = 3
+ContadorDeFases = 0
+continuar = True
+nome = input("\nDigite seu nome:")
+Boasorte = print("\nBoa Sorte", nome, "!\n")
+
+def jogo(perguntasFeitas,contadorDePulos,ContadorDeFases):
+    AcertoFases = []
+    ContadorDeAcertos = 0
+    
+    while ContadorDeFases != 5:
+        if ContadorDeFases == 0:
+            print("{}ª Fase".format(ContadorDeFases+1))
+            print("Digite P para Pular.")
+            print("Você Ainda têm {} Pulos ".format (contadorDePulos))
+            sorteia = random.randrange(5)
+            perguntasFeitas.append(perguntasNivelA[sorteia])
+            print(perguntasNivelA[sorteia])
+            print(alternativaNivelA[sorteia])
+            resposta = input("Digite sua alternativa: ").upper()
+            
+            while resposta == "P" and contadorDePulos > 0:
+                contadorDePulos -= 1
+                print("{}ª Fase".format(ContadorDeFases + 1))
+                sorteia = random.randrange(5)
+                
+                while perguntasNivelA[sorteia] in perguntasFeitas:
+                    sorteia = random.randrange(5)
+                perguntasFeitas.append(perguntasNivelA[sorteia])
+                print(perguntasNivelA[sorteia])
+                print(alternativaNivelA[sorteia])
+                print("Digite P para Pular.")
+                print("Você Ainda têm {} Pulos ".format (contadorDePulos))
+                resposta = input("Digite sua alternativa: ").upper()
+                
+            while contadorDePulos == 0 and resposta == "P":
+                print("Você não possui mais pulos!\n")
+                resposta = input("Digite sua resposta: ").upper()
+            if resposta == respostasNivelA[sorteia]:
+                print("Você Acertou!\n")
+                ContadorDeAcertos = ContadorDeAcertos +1
+                ContadorDeFases = ContadorDeFases +1
+            else:
+                print("Você errou!\n")
+                AcertoFases.append(ContadorDeAcertos)
+                AcertoFases.append(ContadorDeFases)
+                return AcertoFases
+            
+        if ContadorDeFases == 1:
+            print("{}ª Fase".format(ContadorDeFases + 1))
+            print("Digite P para Pular.")
+            print("Você Ainda têm {} Pulos ".format (contadorDePulos))
+            sorteia = random.randrange(5)
+            perguntasFeitas.append(perguntasNivelB[sorteia])
+            print(perguntasNivelB[sorteia])
+            print(alternativaNivelB[sorteia])
+            resposta = input("Digite sua resposta: ").upper()
+            
+            while resposta == "P" and contadorDePulos > 0:
+                contadorDePulos -= 1
+                print("{}ª Fase".format(ContadorDeFases + 1))
+                sorteia = random.randrange(5)
+                
+                while perguntasNivelB[sorteia] in perguntasFeitas:
+                    sorteia = random.randrange(5)
+                perguntasFeitas.append(perguntasNivelB[sorteia])
+                print(perguntasNivelB[sorteia])
+                print(alternativaNivelB[sorteia])
+                print("Digite P para Pular.")
+                print("Você Ainda têm {} Pulos ".format (contadorDePulos))
+                resposta = input("Digite sua resposta: ").upper()
+                
+            while contadorDePulos == 0 and resposta == "P":
+                print("Você não possui pulos!\n")
+                resposta = input("Digite sua resposta: ").upper()
+            if resposta == respostasNivelB[sorteia]:
+                print("Você Acertou!\n")
+                ContadorDeAcertos = ContadorDeAcertos +1
+                ContadorDeFases = ContadorDeFases +1
+            else:
+                print("Você errou!\n")
+                AcertoFases.append(ContadorDeAcertos)
+                AcertoFases.append(ContadorDeFases)
+                return AcertoFases
+
+        if ContadorDeFases == 2:
+            print("{}ª Fase".format(ContadorDeFases + 1))
+            print("Digite P para Pular.")
+            print("Você Ainda têm {} Pulos ".format (contadorDePulos))
+            sorteia = random.randrange(5)
+            perguntasFeitas.append(perguntasNivelC[sorteia])
+            print(perguntasNivelC[sorteia])
+            print(alternativaNivelC[sorteia])
+            resposta=input("Digite sua resposta: ").upper()
+            
+            while resposta == "P" and contadorDePulos > 0:
+                contadorDePulos -= 1
+                print("{}ª Fase".format(ContadorDeFases + 1))
+                sorteia = random.randrange(5)
+                
+                while perguntasNivelC[sorteia] in perguntasFeitas:
+                    sorteia = random.randrange(5)
+                perguntasFeitas.append(perguntasNivelC[sorteia])
+                print(perguntasNivelC[sorteia])
+                print(alternativaNivelC[sorteia])
+                print("Digite P para Pular.")
+                print("Você Ainda têm {} Pulos ".format (contadorDePulos))
+                resposta = input("Digite sua resposta: ").upper()
+                
+            while contadorDePulos == 0 and resposta == "P":
+                print("Você não possui mais pulos!\n")
+                resposta = input("Digite sua resposta: ").upper()
+            if resposta == respostasNivelC[sorteia]:
+                print("Você Acertou!\n")
+                ContadorDeAcertos = ContadorDeAcertos +1
+                ContadorDeFases = ContadorDeFases +1
+            else:
+                print("Você errou!\n")
+                AcertoFases.append(ContadorDeAcertos)
+                AcertoFases.append(ContadorDeFases)
+                return AcertoFases
+
+        if ContadorDeFases == 3:
+            print("{}ª Fase".format(ContadorDeFases + 1))
+            print("Digite P para Pular.")
+            print("Você Ainda têm {} Pulos ".format (contadorDePulos))
+            sorteia = random.randrange(5)
+            perguntasFeitas.append(perguntasNivelD[sorteia])
+            print(perguntasNivelD[sorteia])
+            print(alternativaNivelD[sorteia])
+            resposta=input("Digite sua resposta: ").upper()
+            
+            while resposta == "P" and contadorDePulos > 0:
+                contadorDePulos -= 1
+                print("{}ª Fase".format(ContadorDeFases + 1))
+                sorteia = random.randrange(5)
+                
+                while perguntasNivelD[sorteia] in perguntasFeitas:
+                    sorteia = random.randrange(5)
+                perguntasFeitas.append(perguntasNivelD[sorteia])
+                print(perguntasNivelD[sorteia])
+                print(alternativaNivelD[sorteia])
+                print("Digite P para Pular.")
+                print("Você Ainda têm {} Pulos ".format (contadorDePulos))
+                resposta = input("Digite sua resposta: ").upper()
+                
+            while contadorDePulos == 0 and resposta == "P":
+                print("Você não possui mais pulos!\n")
+                resposta = input("Digite sua resposta: ").upper()
+            if resposta == respostasNivelD[sorteia]:
+                print("Você Acertou!\n")
+                ContadorDeAcertos = ContadorDeAcertos +1
+                ContadorDeFases = ContadorDeFases +1
+            else:
+                print("Você errou!\n")
+                AcertoFases.append(ContadorDeAcertos)
+                AcertoFases.append(ContadorDeFases)
+                return AcertoFases
+
+        if ContadorDeFases == 4:
+            print("{}ª Fase".format(ContadorDeFases + 1))
+            print("Infelizmente você não poderá usar ajuda nessa etapa.")
+            sorteia = random.randrange(5)
+            print("Digite 0 para desistir:")
+            print(perguntasNivelE[sorteia])
+            print(alternativaNivelE[sorteia])
+            resposta = input("Digite sua resposta: ")
+            if resposta == "0":
+                print("Que pena, você desistiu!\n")
+                print("Você Ganhou 500.000,00 Reais!\n")
+                AcertoFases.append(ContadorDeAcertos)
+                AcertoFases.append(ContadorDeFases)
+                return AcertoFases
+            
+            elif resposta == respostasNivelE[sorteia]:
+                print("Você Acertou!\n")
+                ContadorDeAcertos = ContadorDeAcertos +1
+                ContadorDeFases = ContadorDeFases +1
+                AcertoFases.append(ContadorDeAcertos)
+                AcertoFases.append(ContadorDeFases)
+                return AcertoFases
+            
+            else:
+                print("Você errou!\n")
+                print("Você Ganhou 250.000,00 Reais!\n")
+                ContadorDeFases = ContadorDeFases + 1
+                AcertoFases.append(ContadorDeAcertos)
+                AcertoFases.append(ContadorDeFases)
+                return AcertoFases
+            
+repetir = True
+
+while repetir:
+    AcertoFases = jogo(perguntasFeitas, contadorDePulos, ContadorDeFases)
+    if AcertoFases[1] == 0:
+        print("Você ganhou 500,00 Reais!")
+    else:
+        if AcertoFases[0] == 1:
+            print("Você Ganhou 5.000,00 Reais!")
+        elif AcertoFases[0] == 2:
+            print("Você Ganhou 50.000,00 Reais!")
+        elif AcertoFases[0] == 3:
+            print("Você Ganhou 250.000,00 Reais!")
+        elif AcertoFases[0] == 5:
+            print("Você Ganhou 1.000.000,00 Reais!")
+        elif AcertoFases[0] == 4 and AcertoFases[1] == 5:
+            print("Você Ganhou 500.000,00 Reais!")
+    pergunta=input("Dseja jogar novamente? [S|N] ").upper()
+    nome = input("\nDigite seu nome:")
+    Boasorte = print("\nBoa Sorte", nome, "!\n")
+    if pergunta == "N":
+        repetir = False
+    
